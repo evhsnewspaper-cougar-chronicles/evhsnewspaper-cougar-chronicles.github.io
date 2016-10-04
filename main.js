06f77be02d14dd5e956e4815c878c857a19e2b7e
Michelle Li <michellecarolynclark@gmail.com>
2016-10-03 22:59:42 -0700

    slider works (kinda)

--- main.js
+++ main.js
@@ -10,3 +10,14 @@ $(document).ready(function(){
            }
       );
     }); 
+ $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+// Pause slider
+$('.slider').slider('pause');
+// Start slider
+$('.slider').slider('start');
+// Next slide
+$('.slider').slider('next');
+// Previous slide
+$('.slider').slider('prev');
