6ba00cb8ab9f5c028692ccd5a7b56eb1df5135a0
unknown <jennywen2000@gmail.com>
2016-10-04 17:07:29 -0700

    FIXED NAVBAR AND FOOTER YAYAYAY except it's kinda crude but wtv

---
+++ footer.js
@@ -0,0 +1,26 @@
+document.write('\
+	<footer class="page-footer">\
+		<div class="container">\
+			<div class="row">\
+				<div class="col s4">\
+					<h5 class="white-text">The Cougar Chronicles</h5>\
+					<p class="white-text">Evergreen Valley Newspaper Club</p>\
+				</div>\
+				<div class="col s2">\
+					<h5 class="white-text">Contact</h5>\
+					<ul id="icons">\
+						<li style="display: inline"><a class="fa fa-facebook fa-2x white-text" href="https://www.facebook.com/groups/773517586099193/" target="_blank"></a></li>\
+						&nbsp;&nbsp;&nbsp;&nbsp;\
+						<li style="display: inline"><a class="fa fa-github fa-2x white-text" href="https://github.com/evhsnewspaper-cougar-chronicles/evhsnewspaper-cougar-chronicles.github.io" target="_blank"></a></li>\
+						&nbsp;&nbsp;&nbsp;&nbsp;\
+						<li style="display: inline"><a class="fa fa-envelope fa-2x white-text" href="mailto:evhsnewspaperclub@gmail.com" target="_blank"></a></li>\
+					</ul>\
+				</div>\
+				<div><img src="ad.png" height="96px"></div>\
+			</div>\
+			<div class="footer-copyright">\
+				<div class="container">© Webmasters Michelle Li, Purva Bhattacharjee, Jenny Wen</div>\
+			</div>\
+		</div>\
+	</footer>\
+');
---
+++ navbar.js
@@ -0,0 +1,32 @@
+document.write('\
+		<ul id="dropdown1" class="dropdown-content">\
+			<li><a href="september1617.html">Coming Soon!</a></li>\
+		</ul>\
+		<ul id="dropdown2" class="dropdown-content">\
+			<li><a href="september1516.html">September Edition</a></li>\
+			<li><a href="october1516.html">October Edition</a></li>\
+			<li><a href="november1516.html">November Edition</a></li>\
+			<li><a href="january1516.html">January Edition</a></li>\
+			<li><a href="march1516.html">March Edition</a></li>\
+		</ul>\
+		<nav>\
+			<div class="nav-wrapper">\
+				<div class="card-panel">\
+					<div class="card-panel teal lighten-2">\
+						<img src="logo.jpg" height=64px width=auto class="brand-logo">-->\
+						<ul id="nav-mobile" class="right hide-on-med-and-down">\
+							<li><a href="index.html">Home</a></li>\
+							<li><a href="about.html">About</a></li>\
+							<li><a class="dropdown-button" href="#!" data-activates="dropdown2">2015-2016 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
+							<li><a class="dropdown-button" href="#!" data-activates="dropdown1">2016-2017 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
+							<li><a href="advice.html">Need Advice?</a></li>\
+						</ul>\
+					</div>\
+				</div>\
+			</div>\
+		</nav>\
+		<br>\
+		<br>\
+		<br>\
+		<br>\
+');
