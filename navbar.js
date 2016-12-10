c3a713fd3bb6111647cabec2ecd6e5ef9164a620
unknown <jennywen2000@gmail.com>
2016-12-09 22:39:21 -0800

    added fall colors

--- navbar.js
+++ navbar.js
@@ -1,7 +1,6 @@
 document.write('\
 		<ul id="dropdown1" class="dropdown-content">\
-			<li><a href="september1617.html">September Edition: NOW AVAILABLE!</a></li>\
-			<li><a href="#">October Edition: COMING SOON!</a></li>\
+			<li><a href="september1617.html">Coming Soon!</a></li>\
 		</ul>\
 		<ul id="dropdown2" class="dropdown-content">\
 			<li><a href="september1516.html">September Edition</a></li>\
@@ -13,14 +12,14 @@ document.write('\
 		<nav>\
 			<div class="nav-wrapper">\
 				<div class="card-panel">\
-					<div class="card-panel teal lighten-2">\
+					<div class="card-panel orange darken-3">\
 						<img src="logo.jpg" height=64px width=auto class="brand-logo">-->\
 						<ul id="nav-mobile" class="right hide-on-med-and-down">\
 							<li><a href="index.html">Home</a></li>\
 							<li><a href="about.html">About</a></li>\
 							<li><a class="dropdown-button" href="#!" data-activates="dropdown2">2015-2016 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
 							<li><a class="dropdown-button" href="#!" data-activates="dropdown1">2016-2017 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
-							<li><a class="waves-effect waves-light btn-large" href="advice.html">Need Advice?</a></li>\
+							<li><a class="waves-effect waves-light btn-large orange darken-3" href="advice.html">Need Advice?</a></li>\
 						</ul>\
 					</div>\
 				</div>\
