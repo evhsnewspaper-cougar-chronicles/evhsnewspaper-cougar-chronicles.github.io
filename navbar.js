5fdaaec83e0e3ecde49a2764fd515a011d786d83
unknown <jennywen2000@gmail.com>
2017-01-15 12:21:52 -0800

    got rid of space around navbar

--- navbar.js
+++ navbar.js
@@ -12,18 +12,14 @@ document.write('\
 		</ul>\
 		<nav>\
 			<div class="nav-wrapper">\
-				<div class="card-panel">\
-					<div class="card-panel orange darken-3">\
-						<img src="logo.jpg" height=64px width=auto class="brand-logo">-->\
-						<ul id="nav-mobile" class="right hide-on-med-and-down">\
-							<li><a href="index.html">Home</a></li>\
-							<li><a href="about.html">About</a></li>\
-							<li><a class="dropdown-button" href="#!" data-activates="dropdown2">2015-2016 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
-							<li><a class="dropdown-button" href="#!" data-activates="dropdown1">2016-2017 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
-							<li><a class="waves-effect waves-light btn-large orange darken-3" href="advice.html">Need Advice?</a></li>\
-						</ul>\
-					</div>\
-				</div>\
+				<img src="logo.jpg" height=64px width=auto class="brand-logo">-->\
+				<ul id="nav-mobile" class="right hide-on-med-and-down">\
+					<li><a href="index.html">Home</a></li>\
+					<li><a href="about.html">About</a></li>\
+					<li><a class="dropdown-button" href="#!" data-activates="dropdown2">2015-2016 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
+					<li><a class="dropdown-button" href="#!" data-activates="dropdown1">2016-2017 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
+					<li><a class="waves-effect waves-light btn-large orange darken-3" href="advice.html">Need Advice?</a></li>\
+				</ul>\
 			</div>\
 		</nav>\
 		<br>\
