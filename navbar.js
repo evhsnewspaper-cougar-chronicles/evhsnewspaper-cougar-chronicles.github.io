212d81d304381d290af2175d2b6ceec3e23d5cc3
unknown <jennywen2000@gmail.com>
2017-11-09 23:04:24 -0800

    added sept/oct edition

--- navbar.js
+++ navbar.js
@@ -1,10 +1,13 @@
 document.write('\
-		<ul id="dropdown1" class="dropdown-content">\
-			<li><a href="september1617.html">September Edition</a></li>\
-			<li><a href="octnov1617.html">October/November Edition</a></li>\
-			<li><a href="decjan1617.html">December/January Edition: AVAILABLE NOW!</a></li>\
+		<ul id="dropdown3" class="dropdown-content">\
+			<li><a href="septoct1718.html">September/October Edition</a></li>\
 		</ul>\
 		<ul id="dropdown2" class="dropdown-content">\
+			<li><a href="september1617.html">September Edition</a></li>\
+			<li><a href="octnov1617.html">October/November Edition</a></li>\
+			<li><a href="decjan1617.html">December/January Edition</a></li>\
+		</ul>\
+		<ul id="dropdown1" class="dropdown-content">\
 			<li><a href="september1516.html">September Edition</a></li>\
 			<li><a href="october1516.html">October Edition</a></li>\
 			<li><a href="november1516.html">November Edition</a></li>\
@@ -17,8 +20,9 @@ document.write('\
 				<ul id="nav-mobile" class="right hide-on-med-and-down">\
 					<li><a href="index.html">Home</a></li>\
 					<li><a href="about.html">About</a></li>\
-					<li><a class="dropdown-button" href="#!" data-activates="dropdown2">2015-2016 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
-					<li><a class="dropdown-button" href="#!" data-activates="dropdown1">2016-2017 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
+					<li><a class="dropdown-button" href="#!" data-activates="dropdown1">2015-2016 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
+					<li><a class="dropdown-button" href="#!" data-activates="dropdown2">2016-2017 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
+					<li><a class="dropdown-button" href="#!" data-activates="dropdown3">2017-2018 Newspapers<i class="material-icons right">arrow_drop_down</i></a></li>\
 					<li><a class="waves-effect waves-light btn-large #00acc1 cyan darken-1" href="advice.html">Need Advice?</a></li>\
 				</ul>\
 			</div>\
